a8398ecf103340d3672ae0ce60298148f0f44140
CelularPrism <46044765+CelularPrism@users.noreply.github.com>
2021-10-02 12:13:57 +0700

    02.10.2021 12:13

--- Skypunk/Assets/Scripts/Movement.cs
+++ Skypunk/Assets/Scripts/Movement.cs
@@ -6,10 +6,14 @@ public class Movement : MonoBehaviour
 {
     Vector3 oldPos;
     [SerializeField] private Camera cam;
+    [SerializeField] private LayerMask layerMask;
+    [SerializeField] private GameObject scene;
 
+    public float radius = 0.1f;
     public float speed = 0.5f;
-    public float touchPosX = 0;
-    public float touchPosY = 0;
+
+    float touchPosX = 0;
+    float touchPosY = 0;
 
     void Start()
     {
@@ -19,17 +23,50 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Mouse0))
+        Vector3 scale = new Vector3(transform.position.x, transform.position.y);
+        Vector2 nowPos = cam.ScreenToWorldPoint(Input.mousePosition);
+
+        if (Input.GetKey(KeyCode.Mouse0) &&
+            nowPos.x < scale.x + transform.localScale.x / 2 && nowPos.x > scale.x - transform.localScale.x / 2 &&
+            nowPos.y < scale.y + transform.localScale.y / 2 && nowPos.y > scale.y - transform.localScale.y / 2)
         {
-            Vector2 nowPos = cam.ScreenToWorldPoint(Input.mousePosition);
 
             float nowPosX = nowPos.x;
             float nowPosY = nowPos.y;
 
             transform.position = new Vector3(nowPosX, nowPosY);
-        } else
+        } else if (Physics.CheckSphere(transform.position, radius, layerMask) && Input.GetMouseButtonUp(0)) 
         {
-            transform.position = oldPos;
+            Collider[] collider2D = Physics.OverlapSphere(transform.position, radius, layerMask);
+            float minDist = 100f;
+
+            foreach (Collider i in collider2D)
+            {
+                float dist = Vector3.Distance(transform.position, i.transform.position);
+                if (dist < minDist)
+                {
+                    Debug.Log(i.transform.gameObject.name);
+                    minDist = dist;
+                }
+            }
+
+            Collider[] deleteCollider2D = Physics.OverlapBox(transform.position, new Vector2(50f, 2f), Quaternion.Euler(0, 0, 0), layerMask);
+            foreach (Collider i in deleteCollider2D)
+            {
+                Destroy(i.gameObject);
+            }
+
+            foreach (Transform i in scene.transform)
+            {
+                float moveX = i.position.x;
+                float moveY = i.position.y - 3f;
+
+                i.position = new Vector3(moveX, moveY);
+            }
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, oldPos, speed);
         }
     }
 }
