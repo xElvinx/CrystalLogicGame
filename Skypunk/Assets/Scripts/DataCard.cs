85a6ce041fbc9d657299ac1b6511322bcceb5773
CelularPrism <46044765+CelularPrism@users.noreply.github.com>
2021-10-29 21:41:19 +0700

    29.10.2021 CellularPrism6 21:41

---
+++ Skypunk/Assets/Scripts/DataCard.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "New Card", menuName = "Scriptable objects/Card")]
+public class DataCard : ScriptableObject
+{
+    public Text Damage;
+}
